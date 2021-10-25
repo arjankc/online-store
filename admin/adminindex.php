9dbac469455aef6df2d435f739c5aec38f83be7d
Arjan kc <arjankc@gmail.com>
2021-10-25 19:42:52 +0545

    creating forms for admin panel
    creating forms for admin panel to input data, working on categories form.

--- admin/adminindex.php
+++ admin/adminindex.php
@@ -1,44 +1,41 @@
 <!DOCTYPE html>
 <html>
-<?php
-include('adminfiles/head.php')
-?>
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-<?php
-include('adminfiles/header.php')
-?>
-  <!-- Left side column. contains the logo and sidebar -->
-<?php
-include('adminfiles/aside.php');
-?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Dashboard
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-<h1>hello world</h1>
-
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-<?php
-include('adminfiles/footer.php');
-?>
-</body>
+  <?php
+    include('adminfiles/head.php')
+    ?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include('adminfiles/header.php')
+        ?>
+      <!-- Left side column. contains the logo and sidebar -->
+      <?php
+        include('adminfiles/aside.php');
+        ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+          </div>
+        </section>
+        <!-- /.content -->
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <?php
+      include('adminfiles/footer.php');
+      ?>
+  </body>
 </html>
---
+++ admin/categories.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <?php
+    include('adminfiles/head.php')
+    ?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include('adminfiles/header.php')
+        ?>
+      <!-- Left side column. contains the logo and sidebar -->
+      <?php
+        include('adminfiles/aside.php');
+        ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox"> Check me out
+                  </label>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </section>
+        <!-- /.content -->
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <?php
+      include('adminfiles/footer.php');
+      ?>
+  </body>
+</html>
