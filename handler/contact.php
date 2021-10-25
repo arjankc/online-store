20661e5a20c03628d8c6b6c537af90a309f2a8fb
Arjan kc <arjankc@gmail.com>
2021-10-25 19:03:55 +0545

    Update contact.php
    forgot to add the commas 😁

--- handler/contact.php
+++ handler/contact.php
@@ -3,7 +3,7 @@ include('../files/connect.php');
 $email=$_POST['email'];
 $msg=$_POST['msg'];
 
-$sql=INSERT INTO contact(email,msg) VALUES('$email',$msg);
+$sql="INSERT INTO contact(email,msg) VALUES('$email','$msg')";
 $connect->query($sql);
 
 ?>
