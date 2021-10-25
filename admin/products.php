6c7e21edaa1211d71fbe0ee14c65e9027de644be
Arjan kc <arjankc@gmail.com>
2021-10-25 21:51:24 +0545

    category page front end
    adding category page, made a copy for products page as well.

--- admin/products.php
+++ admin/products.php
@@ -34,11 +34,24 @@
             <div class="col-sm-6"> 
 
             <form role="form">
-              <h1> Categories </h1>
               <div class="box-body">
                 <div class="form-group">
-                  <label for="category">Categories</label>
-                  <input type="text" class="form-control" id="category" placeholder="Enter category name">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox"> Check me out
+                  </label>
                 </div>
               </div>
               <!-- /.box-body -->
