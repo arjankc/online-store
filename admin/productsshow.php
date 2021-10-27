68f5636e4301c38242eab0f034ab2e2a3bbc3a9e
Arjan kc <arjankc@gmail.com>
2021-10-27 11:32:21 +0545

    Create productsshow.php
    creating a page to show products in the admin

---
+++ admin/productsshow.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <?php
+  include('adminfiles/session.php');
+    include('adminfiles/head.php')
+    ?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include('adminfiles/header.php')
+        ?>
+      <!-- Left side column. contains the logo and sidebar -->
+      <?php
+        include('adminfiles/aside.php');
+        ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-sm-3">
+              
+            </div>
+            
+        <div class="col-sm-3">
+        </div>
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <?php
+      include('adminfiles/footer.php');
+      ?>
+  </body>
+</html>
