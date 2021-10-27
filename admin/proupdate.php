3a61872476a7d1a2e4fe30841e594b3ab098363a
Arjan kc <arjankc@gmail.com>
2021-10-27 14:02:00 +0545

    page for updating products
    Created page for updating products

---
+++ admin/proupdate.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+  <?php
+  include('adminfiles/session.php');
+    include('adminfiles/head.php')
+    ?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include('adminfiles/header.php')
+        ?>
+      <!-- Left side column. contains the logo and sidebar -->
+      <?php
+        include('adminfiles/aside.php');
+        ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-sm-3">
+              
+            </div>
+            <div class="col-sm-6"> 
+
+            <form role="form" action="producthandler.php" method="post" enctype="multipart/form-data">
+              
+
+
+
+              
+              <h1>Add Product</h1>
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="name">Product name</label>
+                  <input type="text" class="form-control" id="name" placeholder="Enter product name" name="name">
+                </div>
+                <div class="form-group">
+                  <label for="price">Price</label>
+                  <input type="text" class="form-control" id="price" placeholder="Enter amount" name="price">
+                </div>
+                <div class="form-group">
+                  <label for="picture">File input</label>
+                  <input type="file" id="picture" name="file">
+                </div>
+                <div class="form-group">
+                  <label for="description">Product description</label>
+                  <textarea id="description" class="form-control" rows="5" placeholder="Enter description" name="description">
+                  </textarea>
+                </div>
+                <div class="form-group">
+                  <label for="category">Category</label>
+                  <select id="category" name="category">
+                    <?php
+                    $cat="SELECT * from categories";
+                    $results=mysqli_query($connect,$cat);
+                    while($row=mysqli_fetch_assoc($results)){
+                    echo "<option value=".$row['id'].">".$row['name']."</option>";
+                    }
+                    ?>
+                  </select>
+                </div>
+
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          </div>
+        </section>
+        <!-- /.content -->
+        <div class="col-sm-3">
+        </div>
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <?php
+      include('adminfiles/footer.php');
+      ?>
+  </body>
+</html>
