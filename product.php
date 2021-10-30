6f4674b8d4fd8ebf95a91170af02eb89c9a250ee
Arjan kc <arjankc@gmail.com>
2021-10-30 17:16:23 +0545

    fixing the product details
    passing product id for product details page

--- product.php
+++ product.php
@@ -276,7 +276,7 @@
 
 							?>" alt="IMG-PRODUCT">
 
-							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+							<a href="details.php" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
 								Quick View
 							</a>
 						</div>
