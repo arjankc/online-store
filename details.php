e76bb008c5cc60f015ce63fc7f9c33715298a967
Arjan kc <arjankc@gmail.com>
2021-10-30 18:01:02 +0545

    Update details.php
    connecting the details page to the db and the proper variables

--- details.php
+++ details.php
@@ -41,10 +41,20 @@
 								$display = substr($path, 3);
 								echo $display;
 								?>">
-									<div class="wrap-pic-w pos-relative">
-										<img src="images/product-detail-03.jpg" alt="IMG-PRODUCT">
+									<div class="wrap-pic-w pos-relative" style="height:600px">
+										<img src="
+								<?php 
+								$path= $final['picture'];
+								$display = substr($path, 3);
+								echo $display;
+								?>" alt="IMG-PRODUCT">
 
-										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-03.jpg">
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="
+											<?php 
+											$path= $final['picture'];
+											$display = substr($path, 3);
+											echo $display;
+											?>">
 											<i class="fa fa-expand"></i>
 										</a>
 									</div>
@@ -57,15 +67,15 @@
 				<div class="col-md-6 col-lg-5 p-b-30">
 					<div class="p-r-50 p-t-5 p-lr-0-lg">
 						<h4 class="mtext-105 cl2 js-name-detail p-b-14">
-							Lightweight Jacket
+							<?php echo $final['name'] ?>
 						</h4>
 
 						<span class="mtext-106 cl2">
-							$58.79
+							Rs. <?php echo $final['price'] ?>
 						</span>
 
 						<p class="stext-102 cl3 p-t-23">
-							Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+							<?php echo $final['description'] ?>
 						</p>
 						
 						<!--  -->
