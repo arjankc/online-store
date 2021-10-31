91d845cdcc76072c89d47001468f42f89d23ae11
Arjan kc <arjankc@gmail.com>
2021-10-31 22:42:09 +0545

    Update carthandler.php
    checking product status in cart before adding.

--- carthandler.php
+++ carthandler.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 if (isset($_SESSION['cart'])) {
+	$checker=array_column($_SESSION['cart'], 'item_name');
+	if(in_array($_GET['cart_name'], $checker)){
+		echo"<script>
+			alert('Product is already in the cart');
+			window.location.href='product.php';
+		</script>";
+	}
+
 	$count=count($_SESSION['cart']); //counting number of items in the cart
 	$_SESSION['cart'][$count]=array('item_id' => $_GET['cart_id'], 'item_name'=>$_GET['cart_name'],'item_price'=>$_GET['cart_price'] );
 	echo "<script> alert('Product Added');
