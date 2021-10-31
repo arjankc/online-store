00f3e7efabc131cd68847b7465b09d44914e4747
Arjan kc <arjankc@gmail.com>
2021-10-31 23:41:42 +0545

    done
    remove items from cart function, done. 👌

--- cartremove.php
+++ cartremove.php
@@ -2,7 +2,15 @@
 session_start();
 if (isset($_POST['remove'])) {
 	foreach($_SESSION['cart'] as $key => $value) {
-		print_r($key);
+		if ($value['item_name']==$_POST['item_name']) {
+			unset($_SESSION['cart'][$key]);
+			$_SESSION['cart']==array_values($_SESSION['cart']);
+			echo "<script>
+				alert('item removed');
+				window.location.href='cart.php';
+				</script>
+			";
+		}
 
 	}
 }
