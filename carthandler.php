d5cf3c946bafff33e0f955dcb01ead7de4acdce2
Arjan kc <arjankc@gmail.com>
2021-10-31 22:36:08 +0545

    Update carthandler.php
    redirect to product page after product has been added from the details page.

--- carthandler.php
+++ carthandler.php
@@ -3,11 +3,15 @@ session_start();
 if (isset($_SESSION['cart'])) {
 	$count=count($_SESSION['cart']); //counting number of items in the cart
 	$_SESSION['cart'][$count]=array('item_id' => $_GET['cart_id'], 'item_name'=>$_GET['cart_name'],'item_price'=>$_GET['cart_price'] );
-
+	echo "<script> alert('Product Added');
+	window.location.href='product.php';
+	</script>";
 } else {
 	$_SESSION['cart'][0]=array('item_id'=>$_GET['cart_id'],'item_name'=>$_GET['cart_name'],'item_price'=>$_GET['cart_price'] );
+	echo "<script> alert('Product Added');
+	window.location.href='product.php';
+	</script>";
 }
 
-print_r($_SESSION['cart']);
-
+//we can empty the cart with session_destroy()
 ?>
