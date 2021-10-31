9404e7d7a351bc061e17f21f6495d3763dd8fbed
Arjan kc <arjankc@gmail.com>
2021-10-31 23:15:27 +0545

    adding a remove button to the cart
    adding a remove button so cart can be updated

--- cart.php
+++ cart.php
@@ -34,8 +34,8 @@
 						<div class="wrap-table-shopping-cart">
 							<table class="table-shopping-cart">
 								<tr class="table_head">
-									<th class="column-1">Product</th>
-									<th class="column-2"></th>
+									<th class="column-1">Action</th>
+									<th class="column-2">Name</th>
 									<th class="column-3">Price</th>
 									<th class="column-4">Quantity</th>
 									<th class="column-5">Total</th>
@@ -50,11 +50,11 @@
 								<tr class="table_row">
 									<td class="column-1">
 										<div class="how-itemcart1">
-											<img src="images/item-cart-04.jpg" alt="IMG">
+											<button class="btn btn-sm btn-outline-danger">Remove</button>
 										</div>
 									</td>
 									<td class="column-2"><?php echo $value['item_name'] ?></td>
-									<td class="column-3">$ 36.00</td>
+									<td class="column-3">Rs <?php echo $value['item_price'] ?></td>
 									<td class="column-4">
 										<div class="wrap-num-product flex-w m-l-auto m-r-0">
 											<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
@@ -68,7 +68,7 @@
 											</div>
 										</div>
 									</td>
-									<td class="column-5">$ 36.00</td>
+									<td class="column-5"><?php echo $value['item_price'] ?></td>
 								</tr>
 								<?php 
 									}
@@ -80,11 +80,7 @@
 
 						<div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
 							<div class="flex-w flex-m m-r-20 m-tb-5">
-								<input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Coupon Code">
-									
-								<div class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
-									Apply coupon
-								</div>
+								
 							</div>
 
 							<div class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
