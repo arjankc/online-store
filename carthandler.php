eccc7309b6eacb1916378225ca559786701f32da
Arjan kc <arjankc@gmail.com>
2021-10-31 19:46:35 +0545

    Update carthandler.php
    prepping up the cart handler

--- carthandler.php
+++ carthandler.php
@@ -1,10 +1,10 @@
 <?php
-$hello=$_GET['cart_id'];
-
-echo $hello;
-
-
-
+if (isset($_SESSION['cart'])) {
+	$count=count($_SESSION['cart']); //counting number of items in the cart
+	$_SESSION['cart'][$count]=$array('item_id'=>$_GET['cart_id']); //
+} else {
+	$_SESSION['cart'][0]=array('item_id'=>$_GET['cart_id']);
+}
 
 
 ?>
