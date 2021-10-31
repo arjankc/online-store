c009edcb44afa867c18376ac8483c84e4afa5fa0
Arjan kc <arjankc@gmail.com>
2021-10-31 22:22:26 +0545

    adding items to the cart
    sending data to the cart using the add to cart button.

--- carthandler.php
+++ carthandler.php
@@ -1,10 +1,13 @@
 <?php
+session_start();
 if (isset($_SESSION['cart'])) {
 	$count=count($_SESSION['cart']); //counting number of items in the cart
-	$_SESSION['cart'][$count]=$array('item_id'=>$_GET['cart_id']); //
+	$_SESSION['cart'][$count]=array('item_id' => $_GET['cart_id'], 'item_name'=>$_GET['cart_name'],'item_price'=>$_GET['cart_price'] );
+
 } else {
-	$_SESSION['cart'][0]=array('item_id'=>$_GET['cart_id']);
+	$_SESSION['cart'][0]=array('item_id'=>$_GET['cart_id'],'item_name'=>$_GET['cart_name'],'item_price'=>$_GET['cart_price'] );
 }
 
+print_r($_SESSION['cart']);
 
 ?>
