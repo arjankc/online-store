303da6695c05166a4e188b44c7790f11c8a5b132
Arjan kc <arjankc@gmail.com>
2021-11-02 23:41:12 +0545

    Update customerregister.php
    customer registration done

--- handler/customerregister.php
+++ handler/customerregister.php
@@ -4,7 +4,15 @@ $email=$_POST['email'];
 $password=$_POST['password'];
 $password2=$_POST['password2'];
 if ($password==$password2) {
-	echo "correct";
+	$sql="INSERT INTO customers(username, password) VALUES('$email','$password')";
+	$connect->query($sql);
+	echo "<script> alert('Registration successful');
+	window.location.href='../customerforms.php';
+	</script>";
+} else {
+	echo "<script> alert('Password did not match');
+	window.location.href='../customerforms.php';
+	</script>";
 }
 
 
