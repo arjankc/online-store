85e8f7394cadb6d1fa866d76e1cb273ea9968b1d
Arjan kc <arjankc@gmail.com>
2021-11-04 04:54:53 +0545

    Update cart2.php
    forgot to add </div>s 😁

--- cart2.php
+++ cart2.php
@@ -116,17 +116,9 @@
 								</span>
 							</div>
 						</div>
-
-												<div class="flex-w flex-t bor12 p-t-15 p-b-30">
-							<div class="size-208 w-full-ssm">
-								<span class="stext-110 cl2">
-									Shipping:
-								</span>
-							</div>
-
-							<div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
+						<div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
 								<p class="stext-111 cl6 p-t-2">
-									We will deliver anywhere by our Anbu Black Ops.
+									Free delivery all over Nepal!!
 								</p>
 								
 								<div class="p-t-15">
@@ -148,6 +140,12 @@
 										</select>
 										<div class="dropDownSelect2"></div>
 									</div>
+									
+									
+										
+								</div>
+							</div>
+						</div>
 
 						<button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
 							Proceed to Checkout
