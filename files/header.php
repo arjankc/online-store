75b4483dacdb0b91548e26eebb46731839fcd9ed
Arjan kc <arjankc@gmail.com>
2021-11-07 21:35:33 +0545

    Update header.php
    updating the notification product quantity

--- files/header.php
+++ files/header.php
@@ -63,7 +63,16 @@
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m">
 
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11"  onclick="location.href='cart.php'">
+						<?php
+						if (!empty($_SESSION['cart'])) {
+							$qty=count($_SESSION['cart']);?>
+							<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti " onclick="location.href='cart.php'" data-notify="<?php echo $qty ?>">
+						
+						
+
+						<?php }else{?>
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti " onclick="location.href='cart.php'" data-notify="0">
+						<?php } ?>
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 					</div>
@@ -81,10 +90,18 @@
 			<!-- Icon header -->
 			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
 				
-				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10"  onclick="location.href='cart.php'">
-					<i class="zmdi zmdi-shopping-cart"></i>
-				</div>
+				<?php
+						if (!empty($_SESSION['cart'])) {
+							$qty=count($_SESSION['cart']);?>
+							<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti " onclick="location.href='cart.php'" data-notify="<?php echo $qty ?>">
+						
+						
 
+						<?php }else{?>
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti " onclick="location.href='cart.php'" data-notify="0">
+						<?php } ?>
+							<i class="zmdi zmdi-shopping-cart"></i>
+						</div>
 			</div>
 
 			<!-- Button show menu -->
