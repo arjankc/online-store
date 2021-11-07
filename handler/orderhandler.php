5eba93f5a42ea83f0ac6eb9a5e0262ea9e3c228e
Arjan kc <arjankc@gmail.com>
2021-11-07 16:58:42 +0545

    Update orderhandler.php
    working on the orders

--- handler/orderhandler.php
+++ handler/orderhandler.php
@@ -1,13 +1,20 @@
 <?php
-include('../files/connect.php');
 session_start();
+include('../files/connect.php');
 
 $total=$_POST['total'];
 
 $phone=$_POST['phone'];
 
 $address=$_POST['address'];
-
 $customerid=$_SESSION['customerid'];
+
+$sql="INSERT INTO orders(customer_id, address, phone, total) VALUES('$customerid','$address', '$phone', '$total')";
+$connect->query($sql);
+
+$sql2="SELECT id from orders orderby DESC limit 1";
+$result=$connect->query($sql2);
+$final=$result->fetch_assoc();
+$orderid=$final['id'];
 
 ?>
