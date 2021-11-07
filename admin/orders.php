0028edc5d5e224d5527d9c15870460f937d337f1
Arjan kc <arjankc@gmail.com>
2021-11-07 22:39:17 +0545

    duplicate productshow

---
+++ admin/orders.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+  <?php
+  include('adminfiles/session.php');
+    include('adminfiles/head.php')
+    ?>
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include('adminfiles/header.php')
+        ?>
+      <!-- Left side column. contains the logo and sidebar -->
+      <?php
+        include('adminfiles/aside.php');
+        ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            
+            <div class="col-sm-9">
+              <a href="products.php">
+              <button style="color:green;">Add new</button>
+            </a>
+              <?php
+
+              include ('../files/connect.php');
+
+              $sql="SELECT * from products";
+              $results=$connect->query($sql);
+              while($final=$results->fetch_assoc()){ ?>
+                <a href="proshow.php?pro_id=<?php echo $final['id']?>">
+                <h3> <?php echo $final['id'] ?> : <?php echo $final['name'] ?> </h3><br>
+                </a>
+
+                <a href="proupdate.php?up_id=<?php echo $final['id'] ?>">
+                  <button>Update</button>
+                </a>    
+
+                <a href="prodelete.php?del_id=<?php echo $final['id'] ?>">
+                  <button style="color:red">Delete</button>
+                </a> <hr>
+
+              <?php }
+
+              ?>
+
+
+              
+            </div>
+            
+        <div class="col-sm-3">
+        </div>
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <?php
+      include('adminfiles/footer.php');
+      ?>
+  </body>
+</html>
