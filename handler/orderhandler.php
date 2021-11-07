f430626f28e6723172cf98028317bb02fd107810
Arjan kc <arjankc@gmail.com>
2021-11-07 17:09:31 +0545

    Update orderhandler.php
    feeding the orders into the db

--- handler/orderhandler.php
+++ handler/orderhandler.php
@@ -12,9 +12,16 @@ $customerid=$_SESSION['customerid'];
 $sql="INSERT INTO orders(customer_id, address, phone, total) VALUES('$customerid','$address', '$phone', '$total')";
 $connect->query($sql);
 
-$sql2="SELECT id from orders orderby DESC limit 1";
+$sql2="SELECT id from orders order by id DESC limit 1";
 $result=$connect->query($sql2);
 $final=$result->fetch_assoc();
 $orderid=$final['id'];
 
+foreach ($_SESSION['cart'] as $key => $value) {
+	$proid=$value['item_id'];
+	$quantity=$value['quantity'];
+
+	$sql3="INSERT INTO order_details(order_id,product_id,quantity) VALUES('$orderid','$proid','$quantity')";
+	$connect->query($sql3);
+}
 ?>
