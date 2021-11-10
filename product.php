84b08dd95f64cc5a8b034fb7abd9a77ab8b582f6
Arjan kc <arjankc@gmail.com>
2021-11-10 21:06:23 +0545

    updating files
    trying to work on category view

--- product.php
+++ product.php
@@ -16,17 +16,6 @@
 				<div class="flex-w flex-c-m m-tb-10">
 					
 				</div>
-				
-				<!-- Search product -->
-				<div class="dis-none panel-search w-full p-t-10 p-b-15">
-					<div class="bor8 dis-flex p-l-15">
-						<button class="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
-							<i class="zmdi zmdi-search"></i>
-						</button>
-
-						<input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Search">
-					</div>	
-				</div>
 
 			</div>
 
