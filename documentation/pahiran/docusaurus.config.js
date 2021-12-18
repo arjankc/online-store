e69a1166c2dc28b6377e307f2e1d87b5cd78d300
Arjan kc <arjankc@gmail.com>
2021-12-18 11:27:22 +0545

    starting to edit files
    editing documentation files

--- documentation/pahiran/docusaurus.config.js
+++ documentation/pahiran/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Pahiran',
+  tagline: 'CACS301 Project work by Arjan KC',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
