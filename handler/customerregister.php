2e37ee71b84075b48d0bae481a7f63be6e55455e
Arjan kc <arjankc@gmail.com>
2022-10-12 10:12:58 +0545

    Update customerregister.php
    fixed password verification

--- handler/customerregister.php
+++ handler/customerregister.php
@@ -8,7 +8,8 @@ $password2=$_POST['password2'];
 $password_hash=password_hash($password, PASSWORD_BCRYPT);
 $password2_hash=password_hash($password2, PASSWORD_BCRYPT);
 
-if ((password_verify($password, $password_hash))==(password_verify($password2, $password2_hash))) {
+if ((password_verify($password, $password2_hash)) && (password_verify($password2, $password_hash))) 
+{
 	$sql="INSERT INTO customers(username, password) VALUES('$email','$password_hash')";
 	$connect->query($sql);
 	echo "<script> alert('Registration successful');
