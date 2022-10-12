e8b4f2611b5169d38479154652989bbc5022629b
Arjan kc <arjankc@gmail.com>
2022-10-12 08:45:01 +0545

    Update customerregister.php

--- handler/customerregister.php
+++ handler/customerregister.php
@@ -3,8 +3,13 @@ include('../files/connect.php');
 $email=$_POST['email'];
 $password=$_POST['password'];
 $password2=$_POST['password2'];
-if ($password==$password2) {
-	$sql="INSERT INTO customers(username, password) VALUES('$email','$password')";
+
+//encrypting the passwords
+$password_hash=password_hash($password, PASSWORD_BCRYPT);
+$password2_hash=password_hash($password2, PASSWORD_BCRYPT);
+
+if ((password_verify($password, $password_hash))==(password_verify($password2, $password2_hash))) {
+	$sql="INSERT INTO customers(username, password) VALUES('$email','$password_hash')";
 	$connect->query($sql);
 	echo "<script> alert('Registration successful');
 	window.location.href='../customerforms.php';
