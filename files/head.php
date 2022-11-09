f561961cdaee704d1b30216ca77d5a185c6977a9
Arjan kc <arjankc@gmail.com>
2022-11-09 09:33:35 +0545

    Update head.php
    fixed css not found

--- files/head.php
+++ files/head.php
@@ -3,34 +3,34 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="/online-store/images/icons/favicon.png"/>
+	<link rel="icon" type="image/png" href="../online-store/images/icons/favicon.png"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/fonts/linearicons-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/fonts/linearicons-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/slick/slick.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/MagnificPopup/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/MagnificPopup/magnific-popup.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/vendor/perfect-scrollbar/perfect-scrollbar.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/vendor/perfect-scrollbar/perfect-scrollbar.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/online-store/css/util.css">
-	<link rel="stylesheet" type="text/css" href="/online-store/css/main.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../online-store/css/main.css">
 <!--===============================================================================================-->
 <?php
 	session_start();
